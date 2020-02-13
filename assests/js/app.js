9393524d51babdd85af697e11f5e0c4834cee71d
tod619 <trevorodwyer619@hotmail.com>
2020-02-13 12:16:51 +0000

    Hide the banner container by using an onclick event in app.js

---
+++ assests/js/app.js
@@ -0,0 +1,5 @@
+// Add event listenr to the button
+document.querySelector(".banner button").addEventListener("click", () => {
+  //Hide the banner element
+  document.querySelector(".banner").style.display = "none";
+});
