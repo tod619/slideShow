b7154f41f432904cdb55f0b077d79af8f114dff8
tod619 <trevorodwyer619@hotmail.com>
2020-02-13 12:21:18 +0000

    Add js to display slideshow

--- assests/js/app.js
+++ assests/js/app.js
@@ -2,4 +2,8 @@
 document.querySelector(".banner button").addEventListener("click", () => {
   //Hide the banner element
   document.querySelector(".banner").style.display = "none";
+
+  //Display the slideshow
+  document.querySelector(".slideshow-wrapper").style.cssText =
+    "opacity: 1; visibility: visible";
 });
